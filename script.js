449323a206206c99d58d7bd3a6ccc96d587690b1
linhtet1343445 <168333571+linhtet1343445@users.noreply.github.com>
2024-04-28 15:23:11 +0630

    Add files via upload

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ noBtn.addEventListener("mouseover", ()=> {
     const noBtnRect = noBtn.getBoundingClientRect();
     const maxX = window.innerWidth - noBtnRect.width;
     const maxY = window.innerHeight - noBtnRect.height;
-    const ramdomX = Math.floor(Math.ramdom() * maxX)
-    const ramdomY = Math.floor(Math.ramdom() * maxY)
-    noBtn.style.left = ramdomX + "px";
-    noBtn.style.top = ramdomY  + "px";
+    const randomX = Math.floor(Math.random() * maxX)
+    const randomY = Math.floor(Math.random() * maxY)
+    noBtn.style.left = randomX + "px";
+    noBtn.style.top = randomY  + "px";
 })
 
